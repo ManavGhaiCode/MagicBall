381c5597748c61d36278cabac257de00ae98fda1
ManavGhaiCode <manavghai@gmail.com>
2023-05-07 07:29:37 +0530

    add ball hit partical effects

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     private Rigidbody2D rb;
+    private GameObject LastHitObj;
+    private float TimeBetLastCol = 1f;
 
     [SerializeField] private Transform ResetPosition;
+    [SerializeField] private GameObject HitParticleSys;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -12,6 +15,22 @@ public class Ball : MonoBehaviour {
     private void Update() {
         if (rb.velocity.magnitude > 7) {
             rb.velocity = rb.velocity.normalized * 7;
+        }
+        
+        TimeBetLastCol -= Time.deltaTime;
+
+        if (TimeBetLastCol <= 0) {
+            LastHitObj = null;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (LastHitObj != other.gameObject) {
+            Vector2 contact = other.contacts[0].point;
+            Instantiate(HitParticleSys, contact, Quaternion.identity);
+
+            LastHitObj = other.gameObject;
+            TimeBetLastCol = 1f;
         }
     }
 
